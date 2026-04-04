2ec65ef5a7e9fe59d3d9ca3049e537258df5da83
G Vijay <gollavijay9900@gmail.com>
2026-04-04 15:49:41 +0530

    Time: 18 ms (47.33%) | Memory: 46.7 MB (37.39%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,36 +1,29 @@
-public class Solution {
+class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() <= 1) {
-            return s;
-        }
+        int n = s.length();
+        String res = "";
 
-        int maxLen = 1;
-        String maxStr = s.substring(0, 1);
-
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i + maxLen; j <= s.length(); j++) {
-                if (j - i > maxLen && isPalindrome(s.substring(i, j))) {
-                    maxLen = j - i;
-                    maxStr = s.substring(i, j);
-                }
+        for(int i = 0;i<n;i++){
+            // Odd length palindrome
+            int st = i,end = i;
+            while(st>=0&&end<n&&s.charAt(st)==s.charAt(end)){
+                st--;
+                end++;
             }
-        }
+            String temp = s.substring(st+1,end);
+            if(temp.length()>res.length())res=temp;
 
-        return maxStr;
-    }
-
-    private boolean isPalindrome(String str) {
-        int left = 0;
-        int right = str.length() - 1;
-
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
+            // Even length palindrome
+            st = i;
+            end = i+1;
+            while(st>=0&&end<n&&s.charAt(st)==s.charAt(end)){
+                st--;
+                end++;
             }
-            left++;
-            right--;
+            temp = s.substring(st+1,end);
+            if(temp.length()>res.length())res=temp;
         }
 
-        return true;
+        return res;
     }
 }
