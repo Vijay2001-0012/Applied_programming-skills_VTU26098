2038a5b0f22df842e65021a9a0313a97c2f16f8d
G Vijay <gollavijay9900@gmail.com>
2026-04-04 14:01:39 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (89.62%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.java
+++ 898-transpose-matrix/transpose-matrix.java
@@ -1,15 +1,33 @@
 class Solution {
-    public int[][] transpose(int[][] matrix) {
-        int n = matrix.length;
-        int transpose[][] = new int[matrix[0].length][n];
-        for(int i = 0;i < n;i++){
-            for(int j = 0;j<matrix[0].length;j++){
-                transpose[j][i] = matrix[i][j];
+    public int[][] transpose(int[][] mat) {
+        int n = mat.length, m = mat[0].length;
 
+        //non square matrix -> new matrix
+        if(n != m) {
+            int[][] tMat = new int[m][n];
+
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < m; j++) {
+                
+                    tMat[j][i] = mat[i][j];
+                
+                }
+            }
+        return tMat;
+        }
+
+        //square matrix -> no new matrix req
+        for(int i = 0; i < n; i++) {
+            for(int j = i+1; j < n; j++) {
+                if(j > i) {
+                        //swap
+                        int temp = mat[i][j];
+                        mat[i][j] = mat[j][i];
+                        mat[j][i] = temp;
+                }
             }
         }
-        return transpose;
-
         
+        return mat;
     }
 }
