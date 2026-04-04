9b31649844a7370b76c5290fb2514a4b6b485572
G Vijay <gollavijay9900@gmail.com>
2026-04-04 15:42:00 +0530

    Time: 9 ms (93.79%) | Memory: 47 MB (50.97%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,40 +1,36 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> list=new ArrayList<>();
-        HashMap<Character,Integer> map=new HashMap<>();
-        char[] ch = s.toCharArray();
-        char[] c = p.toCharArray();
-        for(char n:c){
-            map.put(n,map.getOrDefault(n,0)+1);
+        List<Integer> list = new ArrayList<>();
+        if(s.length() < p.length()) {
+            return list;
         }
-        int i=0;
-        int j=0;
-        int k=p.length();
-        int count=map.size();
 
-        while(j<ch.length){
-            if(map.containsKey(ch[j])){
-                map.put(ch[j],map.get(ch[j]) - 1);
-                if(map.get(ch[j])==0){
-                    count--;
-                }
+        int[] freqS = new int[26];
+        int[] freqP = new int[26];
+
+
+        for(int i=0; i<p.length(); i++) {
+            freqS[s.charAt(i)-'a']++;
+            freqP[p.charAt(i)-'a']++;
+        }
+
+        if(Arrays.equals(freqS, freqP)) {
+            list.add(0);
+        }
+
+        int start = 0;
+        int end = p.length();
+
+        while(end < s.length()) {
+
+            freqS[s.charAt(start)-'a']--;
+            freqS[s.charAt(end)-'a']++;
+
+            if(Arrays.equals(freqS, freqP)) {
+                list.add(start+1);
             }
-            if(j-i+1<k){
-                j++;
-            }
-            else if(j-i+1==k){
-                if(count==0){
-                    list.add(i);
-                }
-                if(map.containsKey(ch[i])){
-                    map.put(ch[i],map.getOrDefault(ch[i],0)+1);
-                    if(map.get(ch[i]) == 1){
-                        count++;
-                    }
-                }
-                i++;
-                j++;
-            }
+            end++;
+            start++;
         }
         return list;
     }
