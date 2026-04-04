589e364b3ec8700bc73a613d9fed64d71fab32d5
G Vijay <gollavijay9900@gmail.com>
2026-04-04 14:02:43 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (58.72%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.java
+++ 898-transpose-matrix/transpose-matrix.java
@@ -2,7 +2,7 @@ class Solution {
     public int[][] transpose(int[][] mat) {
         int n = mat.length, m = mat[0].length;
 
-        //non square matrix -> new matrix
+       
         if(n != m) {
             int[][] tMat = new int[m][n];
 
@@ -16,11 +16,11 @@ class Solution {
         return tMat;
         }
 
-        //square matrix -> no new matrix req
+
         for(int i = 0; i < n; i++) {
             for(int j = i+1; j < n; j++) {
                 if(j > i) {
-                        //swap
+                       
                         int temp = mat[i][j];
                         mat[i][j] = mat[j][i];
                         mat[j][i] = temp;
