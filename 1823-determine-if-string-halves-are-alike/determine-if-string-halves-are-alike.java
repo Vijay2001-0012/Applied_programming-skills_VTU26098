8084a8abe9a975bc3bea19f79949b2cd87e7abd4
G Vijay <gollavijay9900@gmail.com>
2026-04-04 13:23:27 +0530

    Time: 4 ms (58.87%) | Memory: 43.1 MB (74.30%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -1,25 +1,17 @@
-public class Solution {
+class Solution {
     public boolean halvesAreAlike(String s) {
-        Set<Character> vowels = new HashSet<>();
-        vowels.add('a'); vowels.add('e'); vowels.add('i'); vowels.add('o'); vowels.add('u');
-        vowels.add('A'); vowels.add('E'); vowels.add('I'); vowels.add('O'); vowels.add('U');
+        int n = s.length();
+        int count = 0;
 
-        int length = s.length();
-        int midPoint = length / 2;
+        for (int i = 0; i < n / 2; i++) {
+            if (isVowel(s.charAt(i))) count++;
+            if (isVowel(s.charAt(i + n / 2))) count--;
+        }
 
-        String firstHalf = s.substring(0, midPoint);
-        String secondHalf = s.substring(midPoint);
-
-        return countVowels(firstHalf, vowels) == countVowels(secondHalf, vowels);
+        return count == 0;
     }
 
-    private int countVowels(String str, Set<Character> vowels) {
-        int count = 0;
-        for (char c : str.toCharArray()) {
-            if (vowels.contains(c)) {
-                count++;
-            }
-        }
-        return count;
+    private boolean isVowel(char c) {
+        return "aeiouAEIOU".indexOf(c) != -1;
     }
 }
