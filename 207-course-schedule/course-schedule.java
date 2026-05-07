a03ca0391e025c84272cfd79c6f9228ba8c6b095
G Vijay <gollavijay9900@gmail.com>
2026-05-07 20:16:19 +0530

    Time: 4 ms (93.23%) | Memory: 47.3 MB (32.08%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    public boolean canFinish(int numCourses,
+                             int[][] prerequisites) {
+
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] pre : prerequisites) {
+
+            graph.get(pre[1]).add(pre[0]);
+        }
+
+        int[] visited = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+
+            if (hasCycle(graph, visited, i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean hasCycle(List<List<Integer>> graph,
+                             int[] visited,
+                             int course) {
+
+        if (visited[course] == 1) {
+            return true;
+        }
+
+        if (visited[course] == 2) {
+            return false;
+        }
+
+        visited[course] = 1;
+
+        for (int next : graph.get(course)) {
+
+            if (hasCycle(graph, visited, next)) {
+                return true;
+            }
+        }
+
+        visited[course] = 2;
+
+        return false;
+    }
+}
